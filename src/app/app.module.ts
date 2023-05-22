ca076e829acf3e4b2e1ca65aceb7e71eabc453e3
Leron Charles <lc443@njit.edu>
2023-05-22 15:12:22 -0400

    adding contents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessageModule } from './message/message.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     MessageModule,
-
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: [],
+      }
+    ),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/message/message.module.ts
+++ src/app/message/message.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MessageComponent } from './message.component';
+import { SharedModule } from './shared/shared.module';
+import { EffectsModule } from '@ngrx/effects';
+import { MessageListComponent } from './message-list/message-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MessageComponent, MessageListComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    SharedModule,
+    EffectsModule.forFeature([])
+  ],
 })
-export class MessageModule { }
+export class MessageModule {}
